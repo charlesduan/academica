f9630b76f1d901bb2c98ccb84dfe36980e37ffa8
Charles Duan <cduan@sbf5.com>
2024-11-15 16:00:16 -0500

    Added randomizer and tests

--- lib/academica/testbank.rb
+++ lib/academica/testbank.rb
@@ -1,4 +1,5 @@
 require 'structured'
+require 'academica/testbank/question'
 
 #
 # Represents a test bank of multiple questions.
@@ -6,6 +7,7 @@ require 'structured'
 class TestBank
 
   include Structured
+  include Enumerable
 
   set_description <<~EOF
     A test bank of multiple questions.
@@ -13,6 +15,44 @@ class TestBank
 
   element :questions, [ Question ], description: "List of questions"
 
+
+  def count
+    return @questions.count
+  end
+
+  #
+  # Returns a randomized ordering of questions (which will be stable throughout
+  # the lifetime of a single TestBank object).
+  #
+  def random_map
+    return @random_map if @random_map
+    return @random_map = (0...@questions.count).inject([]) { |memo, qnum|
+      if @questions[qnum].must_follow
+        memo.last.push(qnum)
+      else
+        memo.push([ qnum ])
+      end
+      memo
+    }.shuffle.flatten
+  end
+
+  #
+  # Iterates through each question in the randomly shuffled list. To iterate
+  # questions in the non-shuffled list, access the :questions instance variable.
+  #
+  def each
+    random_map.each do |qnum|
+      yield(@questions[qnum])
+    end
+  end
+
+  #
+  # Returns a question by number in the randomized set.
+  #
+  def [](num)
+    qnum = random_map[num]
+    return qnum && @questions[qnum]
+  end
+
 end
 
-require 'academica/testbank/question'
--- lib/academica/testbank/question.rb
+++ lib/academica/testbank/question.rb
@@ -1,6 +1,46 @@
+require 'academica/testbank/randomizer'
+require 'academica/testbank/randomizable'
+
 class TestBank
 
   class Question
+
+    class ErrorTable
+
+      include Structured
+      set_description <<~EOF
+        An explanation of why choices were wrong. The keys of the hash should be
+        the erroneous choice as a single uppercase letter (which will be
+        automatically parenthesized), and the value an explanation. The
+        explanation should be written as a complete, standalone sentence.
+      EOF
+
+      default_element(
+        RandomizableString,
+        preproc: proc { |s| RandomizableString.new(s) },
+        key: {
+          type:    RandomizableString,
+          preproc: proc { |s| RandomizableString.new("(#{s})") },
+          check:   /\A\([A-Z]\)\z/,
+        },
+        description: <<~EOF
+          The text of an answer explanation. The key should be a single
+          uppercase letter (which will be parenthesized).
+        EOF
+      )
+      def pre_initialize
+        @explanations = {}
+      end
+      def receive_any(key, value)
+        @explanations[key] = value
+      end
+      def add(randomizer)
+        @explanations.each do |key, value|
+          key.add(randomizer)
+          value.add(randomizer)
+        end
+      end
+    end
 
     include Structured
 
@@ -11,21 +51,79 @@ class TestBank
       letter inside parentheses.
     EOF
 
-    element :question, String, description: "The question text"
+    element(
+      :question, RandomizableString, description: "The question text",
+      preproc: proc { |s| RandomizableString.new(s) },
+    )
 
-    default_element String, description: "The text of an answer choice"
+    default_element(
+      RandomizableString,
+      preproc: proc { |s| RandomizableString.new(s) },
+      key: {
+        type:    RandomizableString,
+        preproc: proc { |s| RandomizableString.new("(#{s})") },
+        check:   /\A\([A-Z]\)\z/,
+      },
+      description: <<~EOF
+        The text of an answer choice. The key should be a single uppercase
+        letter (which will be parenthesized).
+      EOF
+    )
 
-    element :answer, String, description: <<~EOF
-      The correct answer choice. The corresponding multiple choice answers
-      should be listed in parentheses. If an explanation is to be provided, it
-      should be given after a period and a space.
-    EOF
+    def receive_any(element, value)
+      @choices[element] = value
+    end
 
-    element :errors, { String => String }, optional: true, description: <<~EOF
-      An explanation of why the other choices were wrong. The keys of the hash
-      should be the erroneous choice, and the value an explanation. The
-      explanation should be written as a complete, standalone sentence.
-    EOF
+    element(
+      :answer, RandomizableString, description: <<~EOF,
+        The correct answer choice. The corresponding multiple choice answers
+        should be listed in parentheses. If an explanation is to be provided, it
+        should be given after a period and a space.
+      EOF
+      preproc: proc { |s| RandomizableString.new(s) },
+    )
+
+    element(
+      :errors, ErrorTable, optional: true,
+      description: 'Table of wrong answer explanations',
+    )
+
+    element(
+      :must_follow, :boolean, optional: true, default: false,
+      description: <<~EOF
+        Whether this question must follow the previous one (for example, because
+        it depends on facts presented in the previous question). This also
+        affects randomization of the names in the question (since the names must
+        match between questions).
+      EOF
+    )
+
+    element(
+      :tags, [ String ], optional: true, default: [].freeze,
+      description: <<~EOF
+        Tags for identifying the subject matter of this question.
+      EOF
+    )
+
+    def pre_initialize
+      @choices = {}
+      @randomizers = []
+    end
+
+    #
+    # Adds a Randomizer to this question, passing it all RandomizableStrings in
+    # this class.
+    #
+    def add(randomizer)
+      @randomizers.push(randomizer)
+      @question.add(randomizer)
+      @choices.each do |key, value|
+        key.add(randomizer)
+        value.add(randomizer)
+      end
+      @answer.add(randomizer)
+      @errors.add(randomizer) if defined? @errors
+    end
 
   end
 end
